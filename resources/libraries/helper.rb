5cbbd27072d9ea3bc6b60a958a25923e9dcf052f
Daniel Castro <dcastro@redborder.com>
2025-08-12 14:36:38 +0100

    lint fixes

--- resources/libraries/helper.rb
+++ resources/libraries/helper.rb
@@ -1,26 +1,24 @@
 module Aerospike
   module Helper
-
     def get_manager_ips(managers_per_service)
       ips = Array(managers_per_service['aerospike']).map do |n|
-        node_obj =
-          if n.is_a?(Hash)
-            n
-          else
-            begin
-              Chef::Node.load(n)
-            rescue => e
-              Chef::Log.warn("Could not load node #{n}: #{e.class}: #{e.message}")
-              nil
-            end
+        node_obj = if n.is_a?(Hash)
+          n
+        else
+          begin
+            Chef::Node.load(n)
+          rescue => e
+            Chef::Log.warn("Could not load node #{n}: #{e.class}: #{e.message}")
+            nil
           end
+        end
 
         next unless node_obj
 
         if node_obj.is_a?(Hash)
-           node_obj['ipaddress_sync'] ||
-           node_obj['ipaddress'] ||
-           (node_obj['automatic'] && node_obj['automatic']['ipaddress'])
+          node_obj['ipaddress_sync'] ||
+            node_obj['ipaddress'] ||
+            (node_obj['automatic'] && node_obj['automatic']['ipaddress'])
         else
           node_obj['ipaddress_sync'] || node_obj['ipaddress']
         end
