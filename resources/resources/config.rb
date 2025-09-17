8d573ff22ed6bffeecc2fc2347cb314ca911b2a2
manegron <manegron@redborder.com>
2025-09-17 14:45:25 +0100

    Fix aerospike

--- resources/resources/config.rb
+++ resources/resources/config.rb
@@ -7,6 +7,5 @@ actions :add, :remove, :register, :deregister
 default_action :add
 
 attribute :user, kind_of: String, default: 'root'
-attribute :ipaddress_sync, kind_of: String, default: '127.0.0.1'
-attribute :aerospike_managers, kind_of: Array, default: ['127.0.0.1']
+attribute :aerospike_ips, kind_of: Array, default: ['127.0.0.1']
 attribute :ipaddress, kind_of: String, default: '127.0.0.1'
